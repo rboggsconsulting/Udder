3e980a0b2d16466f3ac26553c85ec1add3db2432
Richard Boggs <rboggsconsulting@gmail.com>
2020-01-02 11:37:26 -0600

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Richard Boggs on 2020-01-02.
+//  Copyright © 2020 Richard Boggs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
